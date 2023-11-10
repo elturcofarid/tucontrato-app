ab1a4266568aa56a4ef064c09169d5e3c31831eb
Farid Ureche <fureche@icfes.gov.co>
2023-11-09 21:30:43 -0500

    componentes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            <div class="alert alert-{{ $type }}">
-                {{ $message }}
+            <div class="alert alert-">
+                fido dido
             </div>
         </div>
     </body>
